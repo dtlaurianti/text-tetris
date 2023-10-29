221d08b6f871bc43b11fcde8d96a29a80b8e4008
dlaurianti <dlaurianti@uiowa.edu>
2023-10-28 22:43:55 -0500

    feat: blank game board

--- src/tetris.c
+++ src/tetris.c
@@ -5,6 +5,29 @@
 const int WIDTH = 10;
 const int HEIGHT = 20;
 
+char *game_board[12][22];
+
+int draw_game() {
+    for (int row=0; row < HEIGHT+2; row++) {
+        for (int col=0; col < WIDTH+2; col++) {
+            game_board[row][col] = "[]";
+        }
+    }
+    return 0;
+}
+
+int display_game() {
+    for (int row=0; row < HEIGHT+2; row++) {
+        for (int col=0; col < WIDTH+2; col++) {
+            printf("%s", game_board[row][col]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 int main() {
-   return 0; 
+    draw_game();
+    display_game();
+    return 0; 
 }
