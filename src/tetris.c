a18acf3d237d12dd57b8fffe35c04135108bfd13
dlaurianti <dlaurianti@uiowa.edu>
2023-10-28 23:44:12 -0500

    feat: types and constants for tetromino shapes and colors

--- src/tetris.c
+++ src/tetris.c
@@ -2,10 +2,80 @@
 #include <stdio.h>
 #include <math.h>
 
-const int WIDTH = 10;
-const int HEIGHT = 20;
+#define WIDTH 10
+#define HEIGHT 20
 
-char *game_board[12][22];
+typedef struct {
+    int r;
+    int g;
+    int b;
+}RGB;
+
+const RGB I_COLOR = { 0x38, 0x40, 0x9B };
+const RGB O_COLOR = { 0xDB, 0xDB, 0x4C };
+const RGB T_COLOR = { 0xDC, 0x4C, 0x9E };
+const RGB J_COLOR = { 0xDB, 0x8B, 0x45 };
+const RGB L_COLOR = { 0x3E, 0xB5, 0xC9};
+const RGB S_COLOR = { 0x40, 0xAE, 0x7D };
+const RGB Z_COLOR = { 0xDA, 0x3C, 0x40 };
+
+typedef enum {
+    ZERO,
+    NINETY,
+    ONE_EIGHTY,
+    TWO_SEVENTY
+} Orientation;
+
+int I_SHAPE[4][4] = {
+    {0,0,0,0},
+    {1,1,1,1},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+int O_SHAPE[4][4] = {
+    {0,1,1,0},
+    {0,1,1,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+int T_SHAPE[4][4] = {
+    {0,1,0,0},
+    {1,1,1,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+int J_SHAPE[4][4] = {
+    {1,0,0,0},
+    {1,1,1,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+int L_SHAPE[4][4] = {
+    {0,0,1,0},
+    {1,1,1,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+int S_SHAPE[4][4] = {
+    {0,1,1,0},
+    {1,1,0,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+int Z_SHAPE[4][4] = {
+    {1,1,0,0},
+    {0,1,1,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+
+typedef struct {
+    int shape[4][4];
+    RGB color;
+    Orientation orientation;
+}Tetromino;
+
+char *game_board[WIDTH+2][HEIGHT+2];
 
 int draw_game() {
     for (int row=0; row < HEIGHT+2; row++) {
